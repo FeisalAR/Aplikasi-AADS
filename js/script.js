33b07ab86862363f15fc5eb5aca1cb16b3b873fe
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2020-03-04 14:46:53 +0700

    fixes jquery filtering and adds profil ads page

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $("#user-list-search").on("keyup", function () {
         var value = $(this).val().toLowerCase();
         $(".list-group-item").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            $(this).toggle($(this).find('.namaads, .kodeads').text().toLowerCase().indexOf(value) > -1)
         });
     });
 
     $("#program-list-search").on("keyup", function () {
         var value = $(this).val().toLowerCase();
         $(".list-group-item").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            $(this).toggle($(this).find('.namaads, .kodeprogram').text().toLowerCase().indexOf(value) > -1)
         });
     });
 });
