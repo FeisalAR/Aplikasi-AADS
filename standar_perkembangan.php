93510ade86c662e75f2c673652755bb5b7a82049
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2020-07-22 17:43:32 +0700

    fixes standar perkembangan page cut thingy

--- standar_perkembangan.php
+++ standar_perkembangan.php
@@ -137,26 +137,7 @@ $row24 = $stmt->fetchAll();
 
 <body>
 
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5f17f079a45e787d128be688/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
-window.onload = function() {
-    if(!window.location.hash) {
-        window.location = window.location + '#loaded';
-        window.location.reload();
-    }
-}
-</script>
-<!--End of Tawk.to Script-->
+    
 
 
     <img class="dnapic" src="images/dnabg.png" alt="dnapic">
@@ -898,6 +879,22 @@ foreach ($row24 as $rowitems) {
         </div>
     </div>
     </div>
+
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f17f079a45e787d128be688/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
